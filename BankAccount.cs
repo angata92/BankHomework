d4c8107db51b426be3f5c40c6bb11e4f656f2d36
Angel Dimanov <ang_el_@abv.bg>
2018-11-06 16:58:44 +0200

    Add files via upload

---
+++ BankAccount.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankHomework
+{
+    public delegate void WithdrawEventHandler(string accountNumber, decimal balance, decimal amount, bool success);
+
+    public class BankAccount
+    {
+        public event WithdrawEventHandler OnBankAccountChanged;
+
+        public decimal Balance { get; set; }
+
+        public string IBAN { get; set; }
+
+        public BankAccount(string accountNumber, decimal defaultAmount)
+        {
+            this.IBAN = accountNumber;
+
+            this.Balance = defaultAmount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            bool success = false;
+
+            if (this.Balance >= amount)
+            {
+                this.Balance = this.Balance - amount;
+
+                success = true;
+            }
+
+            if (this.OnBankAccountChanged != null)
+            {
+                this.OnBankAccountChanged(this.IBAN, this.Balance, amount, success);
+            }
+        }
+    }
+}
---
+++ MobileCompany.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankHomework
+{
+    public class MobileCompany
+    {
+        private Person[] _clients;
+
+        public MobileCompany(Person[] clients)
+        {
+            this._clients = clients;
+        }
+
+        public void MontlyCharge(string personNumber, string bankAccountNumber, decimal money)
+        {
+            foreach (var client in this._clients)
+            {
+                if (client.PersonalNumber == personNumber)
+                {
+                    foreach (var bankAccount in client.Accounts)
+                    {
+                        if (bankAccount.IBAN == bankAccountNumber)
+                        {
+                            bankAccount.Withdraw(money);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
